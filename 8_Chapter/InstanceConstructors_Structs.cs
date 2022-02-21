ea37d52165fa7d6ca7fb6bf6cd4057994886a9b1
lingsi <wq.lin@winrobot360.com>
2022-02-21 20:30:45 +0800

    值类型构造器

---
+++ 8_Chapter/InstanceConstructors_Structs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLR_Practice._8_Chapter
+{
+
+    public struct Point
+    {
+        public int X, Y;
+        //CLR允许为值类型定义构造器，但是必须显示调用才会执行
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public Point()
+        { 
+            X = Y = 5;
+        }
+    }
+    public class Rectangle
+    {
+        public Point topLeft, bottomRight;
+        public Rectangle()
+        {
+            //这里必须显示调用，如果不调用 也不会为其附上5的初始值
+            topLeft = new Point(10, 10);
+            bottomRight = new Point(20, 20);
+        }
+    }
+
+    public class SomeType
+    {
+        private static int x = 5;
+       
+        public static SomeType operator +(SomeType s1, SomeType s2)
+        {
+            return s1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 using CLR_Practice._7_Chapter;
+using CLR_Practice._8_Chapter;
 
-AnotherType.Main();
+//Rectangle rectangle = new Rectangle();
+//Console.WriteLine(rectangle.topLeft.X);
+//Console.WriteLine(rectangle.topLeft.Y);
+//SomeType some = new SomeType();
+Dictionary<int,string> keyValuePairs = new Dictionary<int,string>();
+Task.Run(async() => {
+    for (int i = 0; i < 100000; i++)
+    {
+        //await Task.Delay(1);
+        keyValuePairs.Add(i, Guid.NewGuid().ToString());
+    }
+    Console.WriteLine("The End");
+});
+
+Task.Run(async() => {
+    await Task.Delay(10);
+    for (int i = 0; i < 1000; i++)
+    {
+        keyValuePairs.TryGetValue(i,out var data);
+        Console.WriteLine(String.Format("{0} = {1}",i,data));
+    }
+});
+
+Console.ReadLine(); 
+
+//AnotherType.Main();
